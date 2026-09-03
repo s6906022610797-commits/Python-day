f76fb864a4270b1dfbabc982b716f899445d658b
Tirawat Ariyapatanakul <s6906022610797@email.kmutnb.ac.th>
2026-09-03 08:22:09 +0700

    Add files via upload

---
+++ lecture 07/SOper2.py
@@ -0,0 +1,14 @@
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
+
+union_set = set1 | set2
+print("union:", union_set)
+
+intersection_set = set1 & set2
+print(set1.intersection(set2))
+
+difference_set = 
+print(set1.difference(set2))
+
+sym_diff_set = 
+print(set1.symmetric_difference(set2))
